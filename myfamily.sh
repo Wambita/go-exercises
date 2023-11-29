b3149c3a0e8bb1421cfe67612bc6f8e2e72617e8
shfana <wambitafana@gmail.com>
2023-11-29 13:24:19 +0300

    removed unneccessary code

--- myfamily.sh
+++ myfamily.sh
@@ -1,4 +1,2 @@
 #! bin/bash
 curl -s https://learn.zone01kisumu.ke/assets/superhero/all.json | jq --argjson my_hero "$HERO_ID" '.[] | select(.id == $my_hero) .connections.relatives' | sed 's/"//g'
-
-#curl https://learn.zone01kisumu.ke/assets/superhero/all.json | jq --argjson my_hero "HERO_ID"  '.[] | select(.id == $my_hero) .connections.relative' | sed 's/""//g'
