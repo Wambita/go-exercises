522b3fbda163a906c7a5fd1f48dae9253a1077d5
shfana <wambitafana@gmail.com>
2023-11-30 03:52:50 +0300

    print combination of numbers

---
+++ printcomb.go
@@ -0,0 +1,21 @@
+package piscine
+
+import "github.com/01-edu/z01"
+
+func PrintComb() {
+	for i := '0'; i <= '9'; i++ {
+		for j := i; j <= '9'; j++ {
+			for k := j; k <= '9'; k++ {
+				z01.PrintRune(i)
+				z01.PrintRune(j)
+				z01.PrintRune(k)
+
+				// Check if it's not the last combination, last possible combination is 789
+				if i != 7 || j != 8 || k != 9 {
+					z01.PrintRune(',')
+					z01.PrintRune(' ')
+				}
+			}
+		}
+	}
+}
