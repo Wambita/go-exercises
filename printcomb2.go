bc9b73855d71dac1f5f756c8547d71c8cd85e1a1
shfana <wambitafana@gmail.com>
2023-11-30 08:47:05 +0300

    print comb2

--- printcomb2.go
+++ printcomb2.go
@@ -7,14 +7,23 @@ import (
 func PrintComb2() {
 	for i := '0'; i <= '9'; i++ {
 		for j := '0'; j <= '9'; j++ {
+			for k := '0'; k <= '9'; k++ {
+				for l := '0'; l <= '9'; l++ {
 			// Check if the digits are unique
-			if i != j || j != i {
+			if i > k || (i == k && j>= 1) != i {
+				continue
+			}
 
 				z01.PrintRune(i)
 				z01.PrintRune(j)
-				
+				z01.PrintRune(' ')
+				z01.PrintRune(i)
+				z01.PrintRune(j)
+				if !(i == '9' && j == '8' && k == '9'){
 					z01.PrintRune(',')
 					z01.PrintRune(' ')
+				}
+					
 				}
 			}
 		}
