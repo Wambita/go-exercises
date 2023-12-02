2187000d06fd82df4635a69364fed406c77f2217
shfana <wambitafana@gmail.com>
2023-12-02 10:29:02 +0300

    piscine

---
+++ printrune.go
@@ -0,0 +1,17 @@
+package main
+
+import "github.com/01-edu/z01"
+
+func main()  {
+	var aRune  string = "Ahh"
+	var aRunelength int= len(aRune)
+	//z01.PrintRune(aRune)
+	//z01.PrintRune('\n')
+	for i:= 0 ; i <= aRunelength ; i++{
+
+		z01.PrintRune(rune(aRune[1]))
+
+	
+	}
+	
+}
---
+++ quad.go
@@ -0,0 +1,20 @@
+package main
+
+import "github.com/01-edu/z01"
+
+func main() {
+	z01.PrintRune('\n')
+	x := 5
+	// y := 3
+	for z := 1; z <= x; z++ {
+		if x == 1 && x != 0 {
+			z01.PrintRune('o')
+		} else if x == 2 && x == x-1 {
+			for a := 2; a <= x-1; a++ {
+				z01.PrintRune('-')
+			}
+		} else if x == x {
+			z01.PrintRune('o')
+		}
+	}
+}
