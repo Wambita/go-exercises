1377d65e5c22b913423b8088cc277ee5169ff907
shfana <wambitafana@gmail.com>
2023-12-05 02:39:03 +0300

    infinte loop

--- printcomb2.go
+++ printcomb2.go
@@ -1,15 +1,16 @@
-/*package piscine
+package piscine
 
 import "github.com/01-edu/z01"
 
 func PrintComb2() {
+	z01.PrintRune('h')
 	/*get 4 numbers loop
 	for i := '0'; i <= '9'; i++ { // first digit first number
 		for j := '0'; j <= '9'; j++ { // second digit first num
 			for k := '0'; k <= '9'; k++ { // first digit second num
 				for l := '0'; l <= '9'; l++ { // second digit second num
 
-					if i != k || j != l { // ensures the first combination is not equal to the second combination
+					if i > k || (i == k && j > 1) { // ensures the first combination is not equal to the second combination
 						z01.PrintRune(i)
 						z01.PrintRune(j)
 						z01.PrintRune(' ')
@@ -31,6 +32,5 @@ func PrintComb2() {
 			}
 		}
 	}
-	z01.PrintRune('\n')
+	z01.PrintRune('\n')*/
 }
-*/
