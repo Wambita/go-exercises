089d0ee7ac5cdf8efb89dbcf661e73bee750a2c0
shfana <wambitafana@gmail.com>
2023-12-05 02:14:24 +0300

    infinte loop

--- iterativefactorial.go
+++ iterativefactorial.go
@@ -6,9 +6,10 @@ func IterativeFactorial(nb int) int {
 	factorial := 1
 	if nb <= 0 || nb >= nb+1 {
 		return 0
-	}
-	for i := 1; i <= nb; i++ {
-		factorial = factorial * i
+	} else {
+		for i := 1; i <= nb; i++ {
+			factorial = factorial * i
+		}
 	}
 	return factorial
 }
