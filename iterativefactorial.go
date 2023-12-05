0dc7c8a8ab570ad7c961f0ab6f3f07d4ff1b80e8
shfana <wambitafana@gmail.com>
2023-12-05 04:15:09 +0300

    infinte loop

--- iterativefactorial.go
+++ iterativefactorial.go
@@ -5,13 +5,13 @@ package piscine
 func IterativeFactorial(nb int) int {
 	factorial := 1
 	if nb == 0 || nb < 0 {
-		return 1
+		return 0
 	} else if nb > 0 && nb <= 23 {
 		for i := 1; i <= nb; i++ {
 			factorial = factorial * i
 		}
 	} else {
-		return 0
+		return 1
 	}
 	return factorial
 }
