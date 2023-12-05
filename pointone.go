50de6ff64b233e059ba29c35d411b36b1119717d
shfana <wambitafana@gmail.com>
2023-12-05 16:32:00 +0300

    pointone

--- pointone.go
+++ pointone.go
@@ -1,5 +1,15 @@
 package piscine
 
+/*import (
+	"fmt"
+)
+
+func main() {
+	n := 0
+	PointOne(&n)   // calls the address of n
+	fmt.Println(n) // prints the value on the address
+}*/
+
 func PointOne(n *int) {
-	*n = 1
+	*n = 1 // stores the address of 1
 }
