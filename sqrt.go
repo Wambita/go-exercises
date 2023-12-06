97384e631cf3f4a5a315b42d89da7bb5346ad3ba
shfana <wambitafana@gmail.com>
2023-12-07 01:19:51 +0300

    square root

---
+++ sqrt.go
@@ -0,0 +1,18 @@
+package piscine
+
+func Sqrt(nb int) int {
+	if nb < 0 { // catch errors  for negative numbers
+		return -1
+	}
+	for i := 1; i <= nb; i++ {
+		if i*i == nb {
+			return i
+		}
+	}
+	return 0
+}
+
+/*func main() {
+	// arg1 := 4
+	fmt.Println(Sqrt(4))
+}*/
