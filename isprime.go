ac2aa0cde3ac5197a155abdeb42bc8856c069406
shfana <wambitafana@gmail.com>
2023-12-07 15:28:42 +0300

    prime numbers

--- isprime.go
+++ isprime.go
@@ -1,18 +1,21 @@
-package piscine
+package main
+
+import "fmt"
 
 func IsPrime(nb int) bool {
 	if nb <= 1 { //  for  nums less than or equal to 1 . negative nums aren't prime. 1 isnt prime
 		return false
+	} else if nb%2 == 0 && nb != 2 { // ensures that the only prime number divisible  by 2 is 2
+		return false
 	}
-	for i := 2; i < nb/2; i++ { //
-		if nb%i == 0 { //
+	for i := 2; i < nb/2; i++ { // starts loop from 2 and iterates from 2 to half the given term
+		if nb%i == 0 { ////checks if n is divisible by the current loop variable value
 			return false
 		}
 	}
 	return true
 }
 
-/*
 func main() {
-	fmt.Println(IsPrime(11))
-}*/
+	fmt.Println(IsPrime(4))
+}
