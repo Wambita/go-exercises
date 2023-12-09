75c64a17db4c570edf30fbc29e22ccff9c7f2a49
shfana <wambitafana@gmail.com>
2023-12-09 23:33:27 +0300

    next prime

--- strrev.go
+++ strrev.go
@@ -1,15 +1,17 @@
 package piscine
 
 func StrRev(s string) string {
-	srune := []rune(s)                 // change string to rune
-	for i := len(s) - 1; i >= 0; i-- { // iterate through loop from last index item to the first
-		return (string(srune[i])) // print rune element at that index
+	srune := []rune(s)                  // change string to rune
+	reverse := make([]rune, len(srune)) // make a slice with length of srune to store reversed string
+	for i := len(s) - 1; i >= 0; i-- {  // iterate through loop from last index item to the first
+		reverse[len(srune)-1-i] = srune[i] // assigns the char at i of the original string to the corresponding in reversed slice
+		// return (string(srune[i])) // print rune element at that index
 	}
-	return ""
+	return string(reverse)
 }
 
 /*
 func main() {
-	StrRev("fana")
+	fmt.Printf(StrRev("fana"))
 }
 */
