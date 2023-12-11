387eaeeb7eac54e2543bebb1c9b25e58b3cc1c85
shfana <wambitafana@gmail.com>
2023-12-12 02:21:11 +0300

    changed function

--- index.go
+++ index.go
@@ -4,21 +4,31 @@ package piscine
 	fmt.Println(Index("Hello!", "l"))
 	fmt.Println(Index("Salut!", "alu"))
 	fmt.Println(Index("Ola!", "hOl"))
-}
-*/
+}*/
+
 func Index(s string, toFind string) int {
 	// returns the index of the first instance of substring return -1 if not present
-	srune := []rune(s)
-	toFindRune := []rune(toFind)
 
-	if len(toFindRune) == 0 {
-		return 0
-	}
+	// calculate lengths of input strings
+	toFindLen := len(toFind)
 
-	for i, value := range srune {
-		if value == toFindRune[0] {
+	// iterate through each possible starting index of a substring in s
+	for i := 0; i <= len(s)-len(toFind); i++ {
+		// variable to track if a match is found for current index
+		match := true
+
+		// iterate through each char to find a substring
+		for j := 0; j < toFindLen; j++ {
+			if s[i+j] != toFind[j] { // check if theres a mismatch
+				match = false
+				break
+			}
+		}
+		// if match is true return i
+		if match {
 			return i
 		}
 	}
+	// return -1 if no match was found
 	return -1
 }
