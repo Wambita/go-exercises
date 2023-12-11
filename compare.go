0b2a31363beb034e5b4ce916e365dd063f4b97a0
shfana <wambitafana@gmail.com>
2023-12-11 23:47:32 +0300

    compare function

---
+++ compare.go
@@ -0,0 +1,17 @@
+package piscine
+
+/*func main() {
+	fmt.Println(Compare("Hello!", "Hello!"))
+	fmt.Println(Compare("Salut!", "lut!"))
+	fmt.Println(Compare("Ola!", "Ol"))
+}*/
+
+func Compare(a, b string) int {
+	if a == b {
+		return 0
+	} else if a < b {
+		return -1
+	} else {
+		return 1
+	}
+}
