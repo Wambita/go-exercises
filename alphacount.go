659b77e4602607545301a154de6ccd066f82c0b2
shfana <wambitafana@gmail.com>
2023-12-12 00:02:56 +0300

    alphacount

---
+++ alphacount.go
@@ -0,0 +1,20 @@
+package main
+
+import "fmt"
+
+func main() {
+	s := "Hello 78 World!    4455 /"
+	nb := AlphaCount(s)
+	fmt.Println(nb)
+}
+
+func AlphaCount(s string) int {
+	srune := []rune(s)
+	count := 0
+	for _, value := range srune {
+		if value >= 'a' && value <= 'z' || value >= 'A' && value <= 'Z' {
+			count++
+		}
+	}
+	return count
+}
