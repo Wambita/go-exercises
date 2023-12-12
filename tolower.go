461b040dbe23fdb85dc4ce95d1e512223cb17708
shfana <wambitafana@gmail.com>
2023-12-12 04:38:12 +0300

    tolower

---
+++ tolower.go
@@ -0,0 +1,18 @@
+package main
+
+/*
+func main() {
+	fmt.Println(ToLower("HAHAHA"))
+}*/
+
+func ToLower(s string) string {
+	srune := []rune(s)
+	for i, value := range srune {
+		if 'A' <= value && value <= 'Z' {
+			// convert to lower case by adding the ascii chars
+			srune[i] = value + ('a' - 'A')
+		}
+	}
+	result := string(srune)
+	return result
+}
