b18469ac801d3f64642f56e5ed6b0a1da2fb4fb1
shfana <wambitafana@gmail.com>
2023-12-13 12:20:02 +0300

    unmatch

---
+++ unmatch.go
@@ -0,0 +1,16 @@
+package piscine
+
+func Unmatch(a []int) int {
+	match := make(map[int]int)
+
+	for _, num := range a {
+		match[num]++
+	}
+
+	for num, count := range match {
+		if count%2 != 0 {
+			return num
+		}
+	}
+	return -1
+}
