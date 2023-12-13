19bc87b84d0ca708fa63ab5e70a1ac38c7f77c9f
shfana <wambitafana@gmail.com>
2023-12-13 15:54:24 +0300

    median

--- abort.go
+++ abort.go
@@ -2,9 +2,9 @@ package piscine
 
 func Abort(a, b, c, d, e int) int {
 	num := []int{a, b, c, d, e}
-	for i := 0; i <= len(num); i++ {
+	for i := 0; i <= len(num)-1; i++ {
 		// sorting in ascending  order
-		for j := i + 1; j < len(num)-1; j++ {
+		for j := i + 1; j < len(num); j++ {
 			if num[i] > num[j] {
 				num[i], num[j] = num[j], num[i]
 			}
