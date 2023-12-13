b02267cca85c29087717cae72377b007546e5537
shfana <wambitafana@gmail.com>
2023-12-13 13:49:45 +0300

    food

---
+++ fooddeliverytime.go
@@ -0,0 +1,30 @@
+package main
+
+/*
+func main() {
+	fmt.Println(FoodDeliveryTime("burger"))
+	fmt.Println(FoodDeliveryTime("soda"))
+	fmt.Println(FoodDeliveryTime("chips"))
+	fmt.Println(FoodDeliveryTime("nuggets"))
+	fmt.Println(FoodDeliveryTime("burger") + FoodDeliveryTime("chips") + FoodDeliveryTime("nuggets"))
+}
+*/
+type food struct {
+	preptime int
+}
+
+func FoodDeliveryTime(order string) int {
+	burger := food{preptime: 15}
+	chips := food{preptime: 10}
+	nuggets := food{preptime: 12}
+
+	if order == "burger" {
+		return burger.preptime
+	} else if order == "chips" {
+		return chips.preptime
+	} else if order == "nuggets" {
+		return nuggets.preptime
+	} else {
+		return 404
+	}
+}
