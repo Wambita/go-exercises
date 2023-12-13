4a06f482e3631db8f813be746441535bb0aa9833
shfana <wambitafana@gmail.com>
2023-12-14 01:01:20 +0300

    shopping list sort

--- shoppinglistsort.go
+++ shoppinglistsort.go
@@ -7,7 +7,6 @@ func main() {
 }
 */
 func ShoppingListSort(slice []string) []string {
-	list := make([]string, 10)
 	for i := 0; i < len(slice)-1; i++ {
 		for j := i + 1; j < len(slice); j++ {
 			if len(slice[i]) > len(slice[j]) {
@@ -15,8 +14,6 @@ func ShoppingListSort(slice []string) []string {
 			}
 		}
 	}
-	for i, item := range slice {
-		list[i] = item
-	}
-	return list
+
+	return slice
 }
