1fbf62c48428db46bccc721cfa6e21fb5e35a0dc
shfana <wambitafana@gmail.com>
2023-12-14 04:05:47 +0300

    display file

---
+++ displayfile/main.go
@@ -0,0 +1,28 @@
+package main
+
+import (
+	"fmt"
+	"io/ioutil"
+	"os"
+)
+
+func main() {
+	// no of args
+	args := os.Args[1:]
+	if len(args) == 0 {
+		fmt.Println("Filename missing")
+	} else if len(args) > 1 {
+		fmt.Println("Too many arguments")
+	} else {
+		// get filename from command line arguments
+		filePath := args[0]
+		// read content of file
+		content, err := ioutil.ReadFile(filePath)
+		if err != nil {
+			fmt.Print("Unable to read file:", err)
+			return
+		}
+		// print content of standard output
+		fmt.Print(string(content))
+	}
+}
