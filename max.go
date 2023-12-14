15655d4576d79173ec2ec7931c1fcb25637fb14c
shfana <wambitafana@gmail.com>
2023-12-14 07:25:34 +0300

    max

---
+++ max.go
@@ -0,0 +1,21 @@
+package main
+
+/*
+func main() {
+	a := []int{23, 123, 1, 11, 55, 93}
+	max := Max(a)
+	fmt.Println(max)
+}
+*/
+func Max(a []int) int {
+	if len(a) == 0 {
+		return 0
+	}
+	max := a[0]
+	for _, num := range a {
+		if num > max {
+			max = num
+		}
+	}
+	return max
+}
