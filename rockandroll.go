99fdc5c3f81f654ff1de03ad60924ec3c6c6d272
shfana <wambitafana@gmail.com>
2023-12-14 20:27:04 +0300

    rock and roll

--- rockandroll.go
+++ rockandroll.go
@@ -11,11 +11,11 @@ func RockAndRoll(n int) string {
 	if n < 0 {
 		return "error: number is negative \n"
 	} else if n%2 == 0 && n%3 == 0 {
-		return "Rock and Roll\n"
+		return "rock and roll\n"
 	} else if n%3 == 0 {
-		return "Roll\n"
+		return "roll\n"
 	} else if n%2 == 0 {
-		return "Rock\n"
+		return "rock\n"
 	} else {
 		return "error: non divisible \n"
 	}
