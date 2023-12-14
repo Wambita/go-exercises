a665e556f35f2a0b2790cac4af790ea18fa60905
shfana <wambitafana@gmail.com>
2023-12-14 20:17:19 +0300

    rock and roll

---
+++ rockandroll.go
@@ -0,0 +1,25 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	fmt.Println(RockAndRoll(4))
+	fmt.Println(RockAndRoll(9))
+	fmt.Println(RockAndRoll(6))
+}
+
+func RockAndRoll(n int) string {
+	if n < 0 {
+		return "error: number is negative \n"
+	} else if n%2 == 0 && n%3 == 0 {
+		return "Rock and Roll\n"
+	} else if n%3 == 0 {
+		return "Roll\n"
+	} else if n%2 == 0 {
+		return "Rock\n"
+	} else {
+		return "error: non divisible \n"
+	}
+}
