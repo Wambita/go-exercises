6613dadb01018b203fc84cf71463b8be4babbd10
shfana <wambitafana@gmail.com>
2023-12-15 06:23:51 +0300

    package

--- rockandroll.go
+++ rockandroll.go
@@ -9,7 +9,7 @@ func main() {
 */
 func RockAndRoll(n int) string {
 	if n < 0 {
-		return "error: number is negative \n"
+		return "error: number is negative\n"
 	} else if n%2 == 0 && n%3 == 0 {
 		return "rock and roll\n"
 	} else if n%3 == 0 {
@@ -17,6 +17,6 @@ func RockAndRoll(n int) string {
 	} else if n%2 == 0 {
 		return "rock\n"
 	} else {
-		return "error: non divisible \n"
+		return "error: non divisible\n"
 	}
 }
