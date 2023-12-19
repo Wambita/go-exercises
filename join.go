0f8734b61bec5e69ac2bef6bee2725459d699c5d
shfana <wambitafana@gmail.com>
2023-12-19 17:50:00 +0300

    join

--- join.go
+++ join.go
@@ -10,10 +10,26 @@ func Join(strs []string, sep string) string {
 	words := ""
 
 	for i, word := range strs {
-		words += word // adds string value to variable
-		if i != len(strs)-1 {
+		words += word         // adds string value to variable
+		if i != len(strs)-1 { // if not last word
 			words += sep // adds separator to string
 		}
 	}
 	return words
 }
+
+/*
+func join2(str []string, sep string) string {
+	// empty string to store final result
+	result := ""
+
+	// loop through string
+	for i, word := range str {
+		result += word
+		if i != len(str)-1 {
+			result += sep
+		}
+	}
+	return result
+}
+*/
