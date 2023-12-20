7499d4359fb042e76049029d2f00e2d4242dcf92
shfana <wambitafana@gmail.com>
2023-12-20 10:08:34 +0300

    concatparams

---
+++ concatparams.go
@@ -0,0 +1,21 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	test := []string{"Hello", "how", "are", "you?"}
+	fmt.Println(ConcatParams(test))
+}
+
+func ConcatParams(args []string) string {
+	var array string          // empty string to store arguments
+	for index := range args { // loop through slice using index
+		array = array + args[index] // add slice elem to arrau
+		if index != len(args)-1 {   // if not last item, separate slice elem with new line
+			array = array + "\n" // add new line after each elem
+		}
+	}
+	return array
+}
