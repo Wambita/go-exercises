e25ca7e007f2e1c3d6504744861e5794832c909d
shfana <wambitafana@gmail.com>
2023-12-21 15:41:13 +0300

    sorted

--- issorted.go
+++ issorted.go
@@ -1,15 +1,18 @@
-package piscine
+package main
 
 /*
 func main() {
 	a1 := []int{0, 1, 2, 3, 4, 5}
 	a2 := []int{0, 2, 1, 3}
+	a3 := []int{801206, 909401, -633231, -821370, 828209, -671399, 125318, -634097}
 
 	result1 := IsSorted(f, a1)
 	result2 := IsSorted(f, a2)
+	result3 := IsSorted(f, a3)
 
 	fmt.Println(result1)
 	fmt.Println(result2)
+	fmt.Println(result3)
 }
 
 // sample func
@@ -19,11 +22,21 @@ func f(a, b int) int {
 */
 // func issorted
 func IsSorted(f func(a, b int) int, a []int) bool {
+	var sorted bool
 	for i := 0; i < len(a)-1; i++ {
-		if f(a[i], a[i+1]) > a[i+1]-a[i] {
+		if f(a[i], a[i+1]) > 0 {
 			// if slice +ve , slice not sorted return false
-			return false
+			sorted = false
 		}
 	}
-	return true
+	if sorted == false {
+		sorted = true
+		for i := 0; i < len(a)-1; i++ {
+			if f(a[i], a[i+1]) < 0 {
+				sorted = false
+			}
+		}
+	}
+
+	return sorted
 }
