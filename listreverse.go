3bc32a7de08162e34993099c21efe16bcebf56e9
shfana <wambitafana@gmail.com>
2023-12-22 04:57:30 +0300

    listeverse

---
+++ listreverse.go
@@ -0,0 +1,60 @@
+package main
+
+/*
+// push new elem at the end of list
+func ListPushBack(l *List, data interface{}) {
+	newNode := &NodeL{Data: data, Next: nil}
+	if l.Head == nil {
+		l.Head = newNode
+		l.Tail = newNode
+	} else {
+		l.Tail.Next = newNode
+		l.Tail = newNode
+	}
+}
+
+func main() {
+	link := &List{}
+
+	ListPushBack(link, 1)
+	ListPushBack(link, 2)
+	ListPushBack(link, 3)
+	ListPushBack(link, 4)
+
+	ListReverse(link)
+
+	it := link.Head
+
+	for it != nil {
+		fmt.Println(it.Data)
+		it = it.Next
+	}
+
+	fmt.Println("Tail", link.Tail)
+	fmt.Println("Head", link.Head)
+}
+
+type NodeL struct {
+	Data interface{}
+	Next *NodeL
+}
+
+type List struct {
+	Head *NodeL
+	Tail *NodeL
+}
+*/
+func ListReverse(l *List) {
+	var previous *NodeL
+	current := l.Head
+
+	for current != nil {
+		next := current.Next
+		current.Next = previous
+
+		previous = current
+		current = next
+	}
+	l.Tail = l.Head
+	l.Head = previous
+}
