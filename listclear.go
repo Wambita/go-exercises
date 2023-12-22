441539ef4cba15600de51f6907819c098bda0491
shfana <wambitafana@gmail.com>
2023-12-22 03:56:12 +0300

    listclear

---
+++ listclear.go
@@ -0,0 +1,55 @@
+package piscine
+
+/*
+// node
+type NodeL struct {
+	Data interface{}
+	Next *NodeL
+}
+
+// list
+type List struct {
+	Head *NodeL
+	Tail *NodeL
+}
+
+func PrintList(l *List) {
+	link := l.Head
+	for link != nil {
+		fmt.Print(link.Data, " -> ")
+		link = link.Next
+	}
+	fmt.Println(nil)
+}
+
+// push new elem at the end of list
+func ListPushBack(l *List, data interface{}) {
+	newNode := &NodeL{Data: data, Next: nil}
+	if l.Head == nil {
+		l.Head = newNode
+		l.Tail = newNode
+	} else {
+		l.Tail.Next = newNode
+		l.Tail = newNode
+	}
+}
+
+func main() {
+	link := &List{}
+
+	ListPushBack(link, "I")
+	ListPushBack(link, 1)
+	ListPushBack(link, "something")
+	ListPushBack(link, 2)
+
+	fmt.Println("------list------")
+	PrintList(link)
+	ListClear(link)
+	fmt.Println("------updated list------")
+	PrintList(link)
+}
+*/
+func ListClear(l *List) {
+	l.Head = nil
+	l.Tail = nil
+}
